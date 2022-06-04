80a12a5cb517bfa2101d2c1ba26045bec30726d6
Smarsden007 <seannmarsden@gmail.com>
2022-06-04 05:22:29 -0700

    WIP

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    
+    <Navbar />
+    
+  
+    
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+
+const Navbar = (props) => {
+    const toggleMode = e => {
+        e.preventDefault();
+        props.setDarkMode(!props.darMode)
+    };
+    return(
+        <nav className="navbar">
+            <h1>Project Blackhawk</h1>
+            <div>
+                Test
+            </div>
+
+
+
+        </nav>
+    )
+
+}
+
+export default Navbar;
